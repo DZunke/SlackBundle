bcf1e2aaf185eb409ff4091a07c83918a87acb6e
Max Kotliar <kotlyar.maksim@gmail.com>
2018-11-09 09:15:37 +0200

    Do not require symfony/symfony package. (#39)

--- DependencyInjection/DZunkeSlackExtension.php
+++ DependencyInjection/DZunkeSlackExtension.php
@@ -4,8 +4,9 @@ namespace DZunke\SlackBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
 
 class DZunkeSlackExtension extends Extension
 {
